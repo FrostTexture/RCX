7972940cf6199d4475f2a65cc4309408b592ceb7
FrostTexture <110172403+FrostTexture@users.noreply.github.com>
2022-07-29 11:05:06 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,25 @@
+getgenv().autoClicker = false
+getgenv().autoRebirth = false
 
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Rebirth Champions X [RCX]", "GrapeTheme")
+
+local a = Window:NewTab("Credits")
+local b = Window:NewTab("Main")
+local c = Window:NewTab("Eggs")
+local d = Window:NewTab("Misc")
+local e = Window:NewTab("Settings")
+
+local a1 = a:NewSection("Autoclicker")
+a1:NewToggle("Autoclicker", "An autoclicker", function(state)
+    if state then
+        getgenv().autoClicker = true
+while getgenv().autoClicker == true do
+   game:GetService("ReplicatedStorage").Events.Click3:FireServer()
+   task.wait()
+end
+    else
+        getgenv().autoClicker = false
+    end
+end)
